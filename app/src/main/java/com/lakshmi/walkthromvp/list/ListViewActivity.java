b0c9662334b08e5e82d6cbb538d6ff82845b1f1f
mgs1899 <lakshmijec@gmail.com>
2017-04-13 19:00:15 +0530

    Second

--- app/src/main/java/com/lakshmi/walkthromvp/list/ListViewActivity.java
+++ app/src/main/java/com/lakshmi/walkthromvp/list/ListViewActivity.java
@@ -1,11 +1,25 @@
 package com.lakshmi.walkthromvp.list;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.widget.ListView;
 
 import com.lakshmi.walkthromvp.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,5 +40,62 @@ public class ListViewActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listactivity);
         ButterKnife.bind(this);
+
+        new CallAsyncTask().execute();
+    }
+
+    private class CallAsyncTask extends AsyncTask<Void,String,String>{
+
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            try {
+                URL url=new URL("https://api.learn2crack.com/android/json/");
+                HttpURLConnection con= (HttpURLConnection) url.openConnection();
+                InputStream is=con.getInputStream();
+                return readStream(is);
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String str) {
+            super.onPostExecute(str);
+
+            Log.d("Lakshmi","This is str  "+str);
+
+
+
+
+        }
+    }
+
+    private String readStream(InputStream in) {
+        BufferedReader reader = null;
+        StringBuffer response = new StringBuffer();
+        try {
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return response.toString();
     }
 }
